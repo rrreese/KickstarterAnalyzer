42ec56b1694e3cdd8b54977fcb0b869362aac695
Robert Anton Reese <robert@rrreese.com>
2012-12-07 21:55:22 +0000

    Adding project

---
+++ KickstarterAnalyzer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KickstarterAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scraper = new Scraper.Scraper();
+
+            // Find pages
+
+            // Download Pages
+
+            // Scrape Pages
+
+            // Analyse
+        }
+    }
+}
